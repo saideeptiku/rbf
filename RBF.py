562c6affc62b1df5c6cd9a3ed32b40f25276dbab
saideep tiku <saitiku@gmail.com>
2017-07-25 03:01:04 -0600

    sync

--- RBF.py
+++ RBF.py
@@ -3,24 +3,25 @@ This file has functions that allow for Rank Based Fingerprinting. IPIN 11/12
 Created By: Machaj, Brida, Piche
 """
 import pandas as pd
+from util_functions import print_df
 
 
 class RBF:
-    def __init__(self, offline_df, input_labels, output_labels, missing_val_marker=float('NaN')):
+    def __init__(self, offline_df, input_labels, output_label):
         """
         init class with a offline_data.
+        Missing access points should be marked with NaN
 
-        offline data: pandas data frame
+        offline_df: pandas data frame
         each row in the dataframe represents, 
         RSSI values and position at a reference point
 
-        wap_labels: list
+        input_labels: list
         labels for columns that represent WAP unique names.
         unique names can be MAC ids.
 
-        pos_labels: list
-        labels that represent the position
-        can be upto 3
+        output_labels: string
+        label that represent the position, values should be integer
 
         missing_val_marker: 
         marker for missing data
@@ -29,46 +30,108 @@ class RBF:
         set to None if not required
 
         member variables:
-        self.ranked_in_df: ranked input dataframe 
+        self.ranked_in_df: ranked input data frame 
 
-        self.in_df : clean input dataframe with only input and output labels
+        self.in_df : clean input data frame with only input and output labels
 
-        self.in_labels: input labels
+        self.inpu_labels: input labels
 
-        self.out_labels: output labels
+        self.pos_lbl: output labels
         """
-
-        # drop useless information from raw data
-        self.offline_df = pd.DataFrame(offline_df[list(input_labels) + list(output_labels)])
 
         # init variables like offline rank vectors
         self.input_labels = input_labels
-        self.output_labels = output_labels
+        self.pos_lbl = output_label
 
         # create df with sorted rows
+        self.offline_sorted = RBF.build_sorted_df(offline_df[list(input_labels) + [output_label]],
+                                                  input_labels,
+                                                  output_label)
 
-        # convert raw data into sorted MAC addresses or unique names
-        # create an identical ranked table with location and rank matrix
-        # self.ranked_in_df = offline_df[input_labels].rank(axis=1, method='dense').join(offline_df[output_labels])
-
-
-    def build_rank_vectors_online(self, online_data):
+    @staticmethod
+    def build_sorted_df(offline_df, input_labels, pos_lbl):
         """
-        create rank vectors for the online data
-
-        online_data: list 1-D
-        RSSI values for different WAP.
-        The order of the RSSI values should match the RSSI labels.
+        convert simple offline data frame into sorted data frame like so:
+        
+        Input DF: self.offline_df should be declared at init
+                  self.output and self.input labels should be declared 
+        WAP_1  WAP_2  WAP_3  pos_label
+          5      4      2        1
+          7      2      5        2
+          2      1      10       3     
+        
+        :return: Sorted data frame that looks like so, columns are ranks, NA have last rank
+          1      2      3      pos_label
+        WAP_1  WAP_2  WAP_3        1
+        WAP_1  WAP_3  WAP_2        2
+        WAP_3  WAP_1  WAP_2        3     
         """
 
-        # TODO: only process common points or ref points
-        #
-        # TODO: create rank vectors for offline phase
-        pass
+        # rank the data frame, so each value is now a rank
+        ranked_df = offline_df[input_labels].rank(axis=1, method='dense') \
+            .join(offline_df[pos_lbl])
 
-    def get_projected_position(K, similarity_measure=""):
+        # print_df(ranked_df)
+
+        # create an empty data frame with columns names as ranks and has position label
+        # columns are ranks and the data entries are column labels from ranked data frame
+        sorted_df = pd.DataFrame(index=ranked_df.index,
+                                 columns=list(range(1, len(input_labels) + 1)) + [pos_lbl])
+
+        # print_df(sorted_df)
+
+        # iterate over each row of ranked data
+        for index in list(ranked_df.index):
+            for ap_name in input_labels:
+                ap_rank = ranked_df.get_value(index, ap_name)
+                pos_num = ranked_df.get_value(index, pos_lbl)
+
+                if pd.isnull(ap_rank):
+                    continue
+
+                # populate the sorted data frame
+                sorted_df.loc[index, ap_rank] = ap_name
+                sorted_df.loc[index, pos_lbl] = pos_num
+
+        # print_df(sorted_df)
+        return sorted_df
+
+    @staticmethod
+    def get_sorted_vector_at_index(index, df, input_labels, pos_lbl):
+        """
+        return ranked WiFi vector that resides at an index on the frame
+        index starts with 1
+        :param input_labels: 
+        :param df: data frame
+        :param index: 
+        :param pos_lbl: position label
+        :return: 
+        ranked WiFi vector that resides at an index on the frame, 
+        position label
+        """
+        if index == 0:
+            exit("index should begin with 1")
+
+        df = df[list(input_labels) + [pos_lbl]]
+
+        row_index_df = df.ix[[index]]
+
+        # df with only one row
+        sorted_df = RBF.build_sorted_df(row_index_df, input_labels, pos_lbl)
+
+        pos = int(sorted_df.loc[1, pos_lbl])
+
+        sorted_vector = list(sorted_df.loc[1])
+
+        # remove NaNs and return
+        return [int(x) for x in sorted_vector if pd.notnull(x)], pos
+
+    def get_projected_position(self, sorted_vector, K, similarity_measure="spearmans_footrule"):
         """
         get the projected position
+
+        ranked_vector: list of ints
+        This is a ranked vector representation of WiFi fingerprint at an unknown position
 
         K: integer
         These are the K reference points used to calculate the reference position
@@ -87,6 +150,7 @@ class RBF:
         # TODO: get distances from similarity measures
 
         # TODO: calculate projected position using value of K
+        print(self.input_labels)
         pass
 
     def test(self):
--- run_rbf.py
+++ run_rbf.py
@@ -1,6 +1,7 @@
 # import modules
 import pandas as pd
 from label_x_y_locations import label_similar_locations
+from RBF import RBF
 from util_functions import print_df
 
 # read data training
@@ -16,13 +17,24 @@ train_df, test_df = label_similar_locations(training_df,
 # make input feature columns list
 input_cols = train_df.columns[1:-4]
 # make output feature column list
-output_cols = train_df.columns[-1]
+output_col = train_df.columns[-1]
 
 print("number of columns total:", len(train_df.columns))
 
 print("input cols:", input_cols)
 print("input col length:", len(input_cols))
 
-print("output cols:", output_cols)
-print("output col length:", len(output_cols))
+print("output cols:", output_col)
+print("output col length:", len(output_col))
+
+# create a model for ranked offline data
+rbf_model = RBF(train_df, input_cols, output_col)
+
+# get the sorted vector and actual position from test data frame
+sorted_vector, actual_position = RBF.get_sorted_vector_at_index(1,
+                                                                train_df,
+                                                                input_cols,
+                                                                output_col)
+
+
 
