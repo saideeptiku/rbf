f44c078d495b06241cf507284fef5652655a550b
saideep tiku <saitiku@gmail.com>
2017-08-02 00:46:37 -0600

    plot

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from RBF import RBF
 from util_functions import print_df
 import matplotlib.pyplot as plt
 
-
 # def main():
 # read data training
 training_df = pd.read_csv("data/bcinfill-s6-run1.csv")
@@ -23,6 +22,7 @@ input_cols = train_df.columns[1:-4]
 # make output feature column list
 pos_label = train_df.columns[-1]
 
+print_df(train_df)
 
 #
 # def run_RBF(train_df, test_df, input_cols, pos_label)
@@ -33,8 +33,11 @@ rbf_model = RBF(train_df, input_cols, pos_label, ['x', 'y'])
 fig = plt.figure()
 ax1 = fig.add_subplot(1, 1, 1)
 
+im = plt.imread("data/bcinfill_map.jpg")
+implot = plt.imshow(im, origin='upper', extent=[-15, 860, 250, 630])
+
 # get position at each training position
-for index in list(test_df.index):
+for index in [x for x in list(test_df.index) if x % 1 == 0]:
     # get the sorted vector and actual position from test data frame
     sorted_vector, actual_pos_label = RBF.get_sorted_vector_at_index(index,
                                                                      test_df,
@@ -42,15 +45,16 @@ for index in list(test_df.index):
                                                                      pos_label)
     # actual x and y position
     real_x, real_y = rbf_model.get_xy_from_position_label(actual_pos_label, test_df)
-    ax1.scatter(real_x, real_y, c='b', marker='o', alpha=0.4)
+    real_pos_plt = ax1.scatter(real_x, real_y, c='b', marker='o', alpha=0.6, label="real position")
 
     # projected position
     pos_x, pos_y = rbf_model.get_projected_position(sorted_vector,
-                                                    k=2,
-                                                    similarity_measure='spearman')
+                                                    k=1,
+                                                    similarity_measure='spearmans_footrule')
 
-    ax1.scatter(pos_x, pos_y, c='r', marker='*', alpha=0.4)
+    proj_pos_plt = ax1.scatter(pos_x, pos_y, c='r', marker='*', alpha=0.6, label="projected position")
 
     print(index, sep="", end=", ", flush=True)
 
+plt.legend(handles=[real_pos_plt, proj_pos_plt], bbox_to_anchor=(0.63, 0.97))
 plt.show()
